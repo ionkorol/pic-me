47232985f0364f30fdc3cbe18ec1aca600d01444
Ion Sobol <ionsobol@gmail.com>
2021-10-20 13:34:31 -0400

    Removed Secrets

--- src/utils/firebase.ts
+++ src/utils/firebase.ts
@@ -3,13 +3,6 @@ import firebase from "firebase";
 import "firebase/auth";
 import "firebase/firestore";
 
-// Optionally import the services that you want to use
-//import "firebase/database";
-//import "firebase/functions";
-//import "firebase/storage";
-
-// Initialize Firebase
-
 if (!firebase.apps.length) {
   firebase.initializeApp(firebaseConfig);
 }
